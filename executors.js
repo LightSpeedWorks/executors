3e72f7f1bae270305f019e7c0b1240fb8ebb27ca
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-12 10:06:12 +0900

    add cancel feature, revise end/close feature

--- executors.js
+++ executors.js
@@ -4,6 +4,7 @@
 	var aa = require('aa'), Channel = aa.Channel;
 
 	var slice = [].slice;
+	var CANCEL_ERROR = new Error('Cancel');
 
 	// Executors
 	function Executors(n) {
@@ -14,13 +15,16 @@
 			throw new TypeError('maximum threads must be a positive finite number');
 
 		var executorChannel = Channel();
+		var closed = false;
+		var canceled = false;
 
 		var allExecutors = aa(startExecutors);
 		executor.end = closeExecutors;
+		executor.cancel = cancelExecutors;
 		return executor;
 
 		// startExecutors
-		function* startExecutors() {
+		function *startExecutors() {
 			var shadowExecutors = [];
 			for (var i = 0; i < n; ++i)
 				shadowExecutors.push(shadowExecutor);
@@ -28,26 +32,37 @@
 		}
 
 		// closeExecutors
-		function* closeExecutors() {
+		function *closeExecutors() {
+			if (closed) return;
+			closed = true;
 			for (var i = 0; i < n; ++i)
 				executorChannel(); // send end of channel to executor's queue
 			yield allExecutors;
 		}
 
+		// cancelExecutors
+		function *cancelExecutors() {
+			if (canceled) return;
+			canceled = true;
+			closeExecutors();
+		}
+
 		// shadowExecutor
-		function* shadowExecutor() {
+		function *shadowExecutor() {
 			var elem;
-			while (elem = yield executorChannel) {
+			while (!canceled && (elem = yield executorChannel)) {
 				try {
 					elem.result(yield elem.fn.apply(elem.ctx, elem.args));
 				} catch (e) {
 					elem.result(e);
 				}
 			}
+			while (elem = yield executorChannel)
+				elem.result(CANCEL_ERROR);
 		}
 
 		// executor
-		function* executor(fn) {
+		function *executor(fn) {
 			var result = Channel();
 			executorChannel({fn:fn, ctx:this, args:slice.call(arguments, 1), result:result});
 			return yield result;
